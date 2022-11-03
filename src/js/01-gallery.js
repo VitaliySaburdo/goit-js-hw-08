f7483fb3b58016f33898fa067ec1395177a7a0be
Vitaliy Saburdo <vsaburdo@gmail.com>
2022-11-03 19:27:01 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryContainerEl = document.querySelector('.gallery')
+const markupGallery = createGallery(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', markupGallery);
+
+function createGallery(cards) {
+    return cards.map(({preview,original,description}) => {return `<div class ="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+        </a>
+        </div>`}).join("");
+}
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
