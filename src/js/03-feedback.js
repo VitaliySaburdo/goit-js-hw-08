eb480a3585d9475fede7db68de546ea4ffbecaf9
VitaliySaburdo <vsaburdo@gmail.com>
2022-11-05 16:43:24 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,7 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 dataFromLocalStorage();
 
